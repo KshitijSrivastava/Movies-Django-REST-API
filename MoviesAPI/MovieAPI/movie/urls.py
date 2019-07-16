d08f7e8250871b0c10c6fbf983917ac13e96c10a
KshitijSrivastava <kshitij127@yahoo.co.in>
2019-07-16 21:25:26 +0530

    Completed Paginated list of movies

--- MoviesAPI/MovieAPI/movie/urls.py
+++ MoviesAPI/MovieAPI/movie/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from movie.views import Home
+from movie.views import Home, movie_list, MovieList
 
 urlpatterns = [
     path('', Home),
+    path('movie/', MovieList.as_view()),
 ]
--- MoviesAPI/MovieAPI/movie/views.py
+++ MoviesAPI/MovieAPI/movie/views.py
@@ -1,7 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
+from movie.serializers import MovieSerializer
+from movie.models import Movie
 # Create your views here.
+
+class PaginationMovie(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
 
 def Home(request):
     return HttpResponse("Hello world!")
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MovieList(generics.ListCreateAPIView, generics.CreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    pagination_class = PaginationMovie
+
+"""
+class MovieList(APIView):
+     def get(self, request, format=None):
+         movies = Movie.objects.all()
+         serializer = MovieSerializer(movies, many=True)
+         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
